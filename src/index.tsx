0cff70217a9dc58548d5d7156ed6578972485636
Pablo Maribondo <pablomgvieira@gmail.com>
2021-05-31 21:20:32 -0300

    chore: craco added

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import '@/firebase/config.ts';
+import 'bulma/css/bulma.min.css';
+
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
