c1b169118f41d67140e26b21a5af1f2b8959077d
Pablo Maribondo <pablomgvieira@gmail.com>
2021-06-02 10:04:21 -0300

    feat: loader component created

---
+++ src/components/UI/Loader.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+const Loader: FC = () => {
+  return (
+    <div className="loader-wrapper">
+      <div className="custom-loader" />
+    </div>
+  );
+};
+
+export default Loader;
