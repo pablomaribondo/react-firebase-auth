4eb6b6df76e9fc735938940df5f97289f590a089
Pablo Maribondo <pablomgvieira@gmail.com>
2021-06-04 21:25:45 -0300

    feat: forgotPassword component created

--- src/components/pages/ForgotPassword.tsx
+++ src/components/pages/ForgotPassword.tsx
@@ -1,42 +1,50 @@
 import { FC, useState, FormEvent, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
 
 import { RootState } from '../../store';
-import { signin, setError } from '../../store/actions/auth.actions';
+import {
+  sendPasswordResetEmail,
+  setError,
+  setSuccess
+} from '../../store/actions/auth.actions';
 import Button from '../UI/Button';
 import Input from '../UI/Input';
 import Message from '../UI/Message';
 
-const SignIn: FC = () => {
+const ForgotPassword: FC = () => {
   const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
   const [isLoading, setIsLoading] = useState(false);
 
   const dispatch = useDispatch();
 
-  const { error } = useSelector((state: RootState) => state.auth);
+  const { error, success } = useSelector((state: RootState) => state.auth);
 
   useEffect(() => {
     return () => {
       if (error) {
         dispatch(setError(''));
       }
-    };
-  }, [error, dispatch]);
 
-  const submitHandler = (event: FormEvent) => {
+      if (success) {
+        dispatch(setSuccess(''));
+      }
+    };
+  }, [error, dispatch, success]);
+
+  const submitHandler = async (event: FormEvent) => {
     event.preventDefault();
     setIsLoading(true);
-    dispatch(signin({ email, password }, () => setIsLoading(false)));
+    await dispatch(sendPasswordResetEmail(email, 'Email sent!'));
+    setIsLoading(false);
   };
 
   return (
     <section className="section">
       <div className="container">
-        <h2 className="has-text-centered is-size-2 mb-3">Sign In</h2>
+        <h2 className="has-text-centered is-size-2 mb-3">Reset Password</h2>
         <form className="form" onSubmit={submitHandler}>
           {error && <Message type="danger" message={error} />}
+          {success && <Message type="success" message={success} />}
           <Input
             type="email"
             name="email"
@@ -45,20 +53,9 @@ const SignIn: FC = () => {
             label="Email address"
             onChange={event => setEmail(event.target.value)}
           />
-          <Input
-            type="password"
-            name="password"
-            value={password}
-            placeholder="Password"
-            label="Password"
-            onChange={event => setPassword(event.target.value)}
-          />
 
-          <p>
-            <Link to="/forgot-password">Forgot password ?</Link>
-          </p>
           <Button
-            text={isLoading ? 'Loading...' : 'Sign In'}
+            text={isLoading ? 'Loading...' : 'Send password reset email'}
             className="is-primary is-fullwidth mt-5"
             disabled={isLoading}
           />
@@ -68,4 +65,4 @@ const SignIn: FC = () => {
   );
 };
 
-export default SignIn;
+export default ForgotPassword;
